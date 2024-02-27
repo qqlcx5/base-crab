b431b7ad1d552449872f0d59c494d3d1eb689eeb
another <qqlcx5@163.com>
2024-02-27 15:52:59 +0800

    vault backup: 2024-02-27 15:00:18

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,11 +1,18 @@
 <template>
     <view class="ca-height-full ca-flex-ajcenter">
         <text class="ca-fs-60 ca-primary">欢迎来到CrabUI</text>
+        <ca-button @click="handleTo">111</ca-button>
     </view>
 </template>
-
+<script setup>
+const handleTo = () => {
+    uni.navigateTo({
+        url: '/pages/mine/index'
+    });
+};
+</script>
 <style>
-page{
+page {
     height: 100vh;
 }
 </style>
