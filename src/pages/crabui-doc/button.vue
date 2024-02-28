8670f67708cfabbc3aacd3d18fd608c0b7f1336d
another <qqlcx5@163.com>
2024-02-28 17:34:12 +0800

    vault backup: 2024-02-27 15:00:18

--- src/pages/crabui-doc/button.vue
+++ src/pages/crabui-doc/button.vue
@@ -1,44 +1,50 @@
 <template>
     <view class="demo-page">
         <view>按钮演示</view>
+        <view class="ca-flex ca-flex-wrap">
+            <!-- 默认按钮 -->
+            <ca-button type="default" @click="onClick">默认按钮</ca-button>
+            <!-- 主题按钮 -->
+            <ca-button type="primary" @click="onClick">主题按钮</ca-button>
+            <!-- 成功按钮 -->
+            <ca-button type="success" @click="onClick">成功按钮</ca-button>
 
-        <!-- 默认按钮 -->
-        <ca-button type="default" @click="onClick">默认按钮</ca-button>
+            <!-- 信息按钮 -->
+            <ca-button type="info" @click="onClick">信息按钮</ca-button>
 
-        <!-- 主题按钮 -->
-        <ca-button type="primary" @click="onClick">主题按钮</ca-button>
+            <!-- 警告按钮 -->
+            <ca-button type="warning" @click="onClick">警告按钮</ca-button>
 
-        <!-- 成功按钮 -->
-        <ca-button type="success" @click="onClick">成功按钮</ca-button>
-
-        <!-- 信息按钮 -->
-        <ca-button type="info" @click="onClick">信息按钮</ca-button>
-
-        <!-- 警告按钮 -->
-        <ca-button type="warning" @click="onClick">警告按钮</ca-button>
-
-        <!-- 危险按钮 -->
-        <ca-button type="danger" @click="onClick">危险按钮</ca-button>
+            <!-- 危险按钮 -->
+            <ca-button type="danger" @click="onClick">危险按钮</ca-button>
+        </view>
 
         <!-- 禁用按钮 -->
-        <ca-button disabled @click="onClick">禁用按钮</ca-button>
+        <ca-button type="primary" disabled @click="onClick">禁用按钮</ca-button>
 
         <!-- 加载按钮 -->
-        <ca-button :loading="true" loading-text="加载中" @click="onClick" />
+        <view class="ca-flex">
+            <ca-button type="primary" :loading="true" width="158" @click="onClick" />
+            <view class="ca-ml-20">
+                <ca-button type="primary" :loading="true" loading-text="加载中..." @click="onClick" />
+            </view>
+        </view>
 
         <!-- 按钮尺寸 -->
         <view class="button-sizes">
-            <ca-button size="mini" @click="onClick">迷你按钮</ca-button>
-            <ca-button size="small" @click="onClick">小按钮</ca-button>
+            <ca-button size="mini" @click="onClick">小号按钮</ca-button>
+            <ca-button size="small" @click="onClick">中号按钮</ca-button>
             <ca-button size="normal" @click="onClick">普通按钮</ca-button>
             <ca-button size="large" @click="onClick">大按钮</ca-button>
         </view>
 
         <!-- 圆形按钮 -->
-        <ca-button circle icon="home" @click="onClick" />
+        <ca-button>
+            圆形按钮
+        </ca-button>
 
         <!-- 带图标的按钮 -->
-        <ca-button type="primary" icon="search" @click="onClick">搜索</ca-button>
+        <ca-button type="primary" icon="ca-sousuo" @click="onClick">搜索</ca-button>
     </view>
 </template>
 <script>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,13 +1,10 @@
 <template>
     <view class="ca-height-full ca-flex-ajcenter">
         <text class="ca-fs-60 ca-primary">欢迎来到CrabUI</text>
-        <ca-button @click="handleTo">111</ca-button>
     </view>
 </template>
 <script setup>
-const handleTo = () => {
 
-}
 </script>
 <style>
 page {
