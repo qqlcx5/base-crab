020d1c1202d4c1723e95f0f912bee804b61b3873
another <qqlcx5@163.com>
2024-03-01 14:36:23 +0800

    vault backup: 2024-02-27 15:00:18

---
+++ src/crabui-doc/componentsA/cell/cell.vue
@@ -0,0 +1,141 @@
+<template>
+    <view class="cell-page">
+        <view class="ca-page__item">
+            <text class="ca-page__item__title">基础功能</text>
+            <ca-cell-group>
+                <ca-cell title="crab UI" value="内容" />
+                <ca-cell title="利剑出鞘" value="内容1111" />
+            </ca-cell-group>
+        </view>
+        <view class="ca-page__item">
+            <text class="ca-page__item__title">自定义图标/图片</text>
+            <ca-cell-group>
+                <ca-cell title="单元格" icon="ca-star" />
+                <ca-cell title="单元格" icon="https://cdn.uviewui.com/uview/example/tag.png" />
+            </ca-cell-group>
+        </view>
+        <view class="ca-page__item">
+            <text class="ca-page__item__title">自定义大小</text>
+            <ca-cell-group size="large">
+                <ca-cell title="单元格" value="内容" />
+                <ca-cell title="单元格" value="内容" label="描述信息" />
+            </ca-cell-group>
+        </view>
+        <view class="ca-page__item">
+            <text class="ca-page__item__title">显示右箭头</text>
+            <ca-cell-group>
+                <ca-cell title="单元格" value="组件" arrow />
+                <ca-cell title="单元格" value="工具" arrow-direction="up" arrow />
+                <ca-cell title="单元格" value="模板" arrow-direction="down" arrow />
+            </ca-cell-group>
+        </view>
+        <view class="ca-page__item">
+            <text class="ca-page__item__title">跳转页面</text>
+            <ca-cell-group>
+                <ca-cell title="打开标签页" arrow url="/pages/componentsB/tag/tag" />
+                <ca-cell title="打开徽标页" arrow url="/pages/componentsB/badge/badge" />
+            </ca-cell-group>
+        </view>
+        <view class="ca-page__item">
+            <text class="ca-page__item__title">右侧内容垂直居中</text>
+            <ca-cell-group>
+                <ca-cell title="单元格" value="内容" label="描述信息" center />
+            </ca-cell-group>
+        </view>
+        <view class="ca-page__item">
+            <text class="ca-page__item__title">自定义插槽</text>
+            <ca-cell-group>
+                <ca-cell value="内容">
+                    <template #title>
+                        <view class="ca-slot-title">
+                            <text class="ca-cell-text">单元格</text>
+                            <ca-tag text="标签" plain size="mini" type="warning" />
+                        </view>
+                    </template>
+                </ca-cell>
+                <ca-cell title="单元格" arrow>
+                    <template #value>
+                        <text class="ca-slot-value">
+                            99
+                        </text>
+                    </template>
+                </ca-cell>
+            </ca-cell-group>
+        </view>
+    </view>
+</template>
+<script>
+export default {
+    data() {
+        return {}
+    },
+    methods: {
+        click() {
+            console.log('Cell is clicked.')
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+.cell-page {
+    padding-bottom: 20px;
+}
+
+.cell-box {
+    &__title {
+        font-size: 14px;
+        color: rgb(143, 156, 162);
+        margin: 20px 0px 0px 15px;
+    }
+
+    &__block {
+        // background-color: #fff;
+        margin-top: 20px;
+    }
+}
+
+.ca-page {
+    padding: 0;
+
+    &__item {
+
+        &__title {
+            color: $color-primary;
+            background-color: $bg-color;
+            padding: 15px;
+            font-size: 15px;
+            display: flex;
+
+            &__slot-title {
+                color: $color-primary;
+                font-size: 14px;
+            }
+        }
+    }
+}
+
+.ca-slot-title {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+}
+
+.ca-cell-text {
+    font-size: 15px;
+    line-height: 22px;
+    color: #303133;
+    margin-right: 5px;
+}
+
+.ca-slot-value {
+    line-height: 17px;
+    text-align: center;
+    font-size: 10px;
+    padding: 0 5px;
+    height: 17px;
+    color: #FFFFFF;
+    border-radius: 100px;
+    background-color: #f56c6c;
+}
+</style>
--- src/crabui-doc/example/components.config.js
+++ src/crabui-doc/example/components.config.js
@@ -33,16 +33,16 @@ export default [
             //     title: 'Text 文本',
             //     title_en: 'Text'
             // },
-            {
-                path: '/pages/componentsC/layout/layout',
-                icon: 'ca-layout',
-                title: 'Layout 布局',
-                title_en: 'Layout'
-            },
+            // {
+            //     path: '/pages/componentsC/layout/layout',
+            //     icon: 'ca-layout',
+            //     title: 'Layout 布局',
+            //     title_en: 'Layout'
+            // },
             {
                 path: '/pages/componentsA/cell/cell',
                 icon: 'ca-cell',
-                title: 'Cell 单元格',
+                title: 'Cell 单元格（nvue组件样式有问题）',
                 title_en: 'Cell'
             },
             {
@@ -99,12 +99,12 @@ export default [
                 title: 'Picker 选择器',
                 title_en: 'Picker'
             },
-            {
-                path: '/pages/componentsC/datetimePicker/datetimePicker',
-                icon: 'ca-datetimePicker',
-                title: 'DatetimePicker 时间选择器',
-                title_en: 'Picker'
-            },
+            // {
+            //     path: '/pages/componentsC/datetimePicker/datetimePicker',
+            //     icon: 'ca-datetimePicker',
+            //     title: 'DatetimePicker 时间选择器',
+            //     title_en: 'Picker'
+            // },
             {
                 path: '/pages/componentsA/rate/rate',
                 icon: 'ca-rate',
@@ -164,109 +164,109 @@ export default [
                 icon: 'ca-switch',
                 title: 'Switch 开关选择器',
                 title_en: 'Switch'
-            },
-            {
-                path: '/pages/componentsB/slider/slider',
-                icon: 'ca-slider',
-                title: 'Slider 滑动选择器',
-                title_en: 'Slider'
-            },
-            {
-                path: '/pages/componentsC/album/album',
-                icon: 'ca-album',
-                title: 'Album 相册',
-                title_en: 'Album'
             }
+            // {
+            //     path: '/pages/componentsB/slider/slider',
+            //     icon: 'ca-slider',
+            //     title: 'Slider 滑动选择器',
+            //     title_en: 'Slider'
+            // },
+            // {
+            //     path: '/pages/componentsC/album/album',
+            //     icon: 'ca-album',
+            //     title: 'Album 相册',
+            //     title_en: 'Album'
+            // }
         ]
     },
     {
         groupName: '数据组件',
         groupName_en: 'Data components',
         list: [
-            {
-                path: '/pages/componentsC/list/list',
-                icon: 'ca-list',
-                title: 'List 列表',
-                title_en: 'List'
-            },
+            // {
+            //     path: '/pages/componentsC/list/list',
+            //     icon: 'ca-list',
+            //     title: 'List 列表',
+            //     title_en: 'List'
+            // },
             {
                 path: '/pages/componentsB/progress/progress',
                 icon: 'ca-progress',
                 title: 'Progress 进度条',
                 title_en: 'Progress'
-            },
+            }
             // {
             // path: '/pages/componentsC/table/table',
             // icon: 'ca-table',
             // title: 'Table 表格（暂无）',
             // title_en: 'Table',
             // },
-            {
-                path: '/pages/componentsB/countDown/countDown',
-                icon: 'ca-countDown',
-                title: 'CountDown 倒计时',
-                title_en: 'CountDown'
-            },
-            {
-                path: '/pages/componentsB/countTo/countTo',
-                icon: 'ca-countTo',
-                title: 'CountTo 数字滚动',
-                title_en: 'CountTo'
-            }
+            // {
+            //     path: '/pages/componentsB/countDown/countDown',
+            //     icon: 'ca-countDown',
+            //     title: 'CountDown 倒计时',
+            //     title_en: 'CountDown'
+            // },
+            // {
+            //     path: '/pages/componentsB/countTo/countTo',
+            //     icon: 'ca-countTo',
+            //     title: 'CountTo 数字滚动',
+            //     title_en: 'CountTo'
+            // }
         ]
     },
     {
         groupName: '反馈组件',
         groupName_en: 'Feedback components',
         list: [
-            {
-                path: '/pages/componentsC/tooltip/tooltip',
-                icon: 'ca-tooltip',
-                title: 'Tooltip 长按提示',
-                title_en: 'ActionSheet'
-            },
-            {
-                path: '/pages/componentsB/actionSheet/actionSheet',
-                icon: 'ca-actionSheet',
-                title: 'ActionSheet 上拉菜单',
-                title_en: 'ActionSheet'
-            },
+            // {
+            //     path: '/pages/componentsC/tooltip/tooltip',
+            //     icon: 'ca-tooltip',
+            //     title: 'Tooltip 长按提示',
+            //     title_en: 'ActionSheet'
+            // },
+            // {
+            //     path: '/pages/componentsB/actionSheet/actionSheet',
+            //     icon: 'ca-actionSheet',
+            //     title: 'ActionSheet 上拉菜单',
+            //     title_en: 'ActionSheet'
+            // },
             {
                 path: '/pages/componentsB/alert/alert',
                 icon: 'ca-alert',
                 title: 'Alert 警告提示',
                 title_en: 'Alert'
             },
-            {
-                path: '/pages/componentsB/toast/toast',
-                icon: 'ca-toast',
-                title: 'Toast 消息提示',
-                title_en: 'Toast'
-            },
-            {
-                path: '/pages/componentsB/noticeBar/noticeBar',
-                icon: 'ca-noticeBar',
-                title: 'NoticeBar 滚动通知',
-                title_en: 'NoticeBar'
-            },
-            {
-                path: '/pages/componentsB/notify/notify',
-                icon: 'ca-notify',
-                title: 'Notify 消息提示',
-                title_en: 'Notify'
-            },
-            {
-                path: '/pages/componentsA/swipeAction/swipeAction',
-                icon: 'ca-swipeAction',
-                title: 'SwipeAction 滑动单元格',
-                title_en: 'SwipeAction'
-            },
-            {
-                path: '/pages/componentsB/collapse/collapse',
-                icon: 'ca-collapse',
-                title: 'Collapse 折叠面板',
-                title_en: 'Collapse'
-            },
+            // {
+            //     path: '/pages/componentsB/toast/toast',
+            //     icon: 'ca-toast',
+            //     title: 'Toast 消息提示',
+            //     title_en: 'Toast'
+            // },
+            // {
+            //     path: '/pages/componentsB/noticeBar/noticeBar',
+            //     icon: 'ca-noticeBar',
+            //     title: 'NoticeBar 滚动通知',
+            //     title_en: 'NoticeBar'
+            // },
+            // {
+            //     path: '/pages/componentsB/notify/notify',
+            //     icon: 'ca-notify',
+            //     title: 'Notify 消息提示',
+            //     title_en: 'Notify'
+            // },
+            // {
+            //     path: '/pages/componentsA/swipeAction/swipeAction',
+            //     icon: 'ca-swipeAction',
+            //     title: 'SwipeAction 滑动单元格',
+            //     title_en: 'SwipeAction'
+            // },
+            // {
+            //     path: '/pages/componentsB/collapse/collapse',
+            //     icon: 'ca-collapse',
+            //     title: 'Collapse 折叠面板',
+            //     title_en: 'Collapse'
+            // },
             {
                 path: '/pages/componentsA/popup/popup',
                 icon: 'ca-popup',
@@ -291,62 +291,62 @@ export default [
         groupName: '布局组件',
         groupName_en: 'Layout components',
         list: [
-            {
-                path: '/pages/componentsC/scrollList/scrollList',
-                icon: 'ca-scrollList',
-                title: 'ScrollList 横向滚动列表',
-                title_en: 'ScrollList'
-            },
-            {
-                path: '/pages/componentsA/line/line',
-                icon: 'ca-line',
-                title: 'Line 线条',
-                title_en: 'Line'
-            },
+            // {
+            //     path: '/pages/componentsC/scrollList/scrollList',
+            //     icon: 'ca-scrollList',
+            //     title: 'ScrollList 横向滚动列表',
+            //     title_en: 'ScrollList'
+            // },
+            // {
+            //     path: '/pages/componentsA/line/line',
+            //     icon: 'ca-line',
+            //     title: 'Line 线条',
+            //     title_en: 'Line'
+            // },
             {
                 path: '/pages/componentsA/overlay/overlay',
                 icon: 'ca-mask',
                 title: 'Overlay 遮罩层',
                 title_en: 'Overlay'
-            },
-            // #ifndef MP-TOUTIAO
-            {
-                path: '/pages/componentsC/noNetwork/noNetwork',
-                icon: 'ca-noNetwork',
-                title: 'NoNetwork 无网络提示',
-                title_en: 'NoNetwork'
-            },
-            // #endif
-            {
-                path: '/pages/componentsA/grid/grid',
-                icon: 'ca-grid',
-                title: 'Grid 宫格布局',
-                title_en: 'Grid'
-            },
-            {
-                path: '/pages/componentsC/swiper/swiper',
-                icon: 'ca-swiper',
-                title: 'Swiper 轮播图',
-                title_en: 'Swiper'
-            },
-            {
-                path: '/pages/componentsC/skeleton/skeleton',
-                icon: 'ca-skeleton',
-                title: 'Skeleton 骨架屏',
-                title_en: 'Skeleton'
-            },
-            {
-                path: '/pages/componentsA/sticky/sticky',
-                icon: 'ca-sticky',
-                title: 'Sticky 吸顶',
-                title_en: 'Sticky'
-            },
-            {
-                path: '/pages/componentsA/divider/divider',
-                icon: 'ca-divider',
-                title: 'Divider 分割线',
-                title_en: 'Divider'
             }
+            // #ifndef MP-TOUTIAO
+            // {
+            //     path: '/pages/componentsC/noNetwork/noNetwork',
+            //     icon: 'ca-noNetwork',
+            //     title: 'NoNetwork 无网络提示',
+            //     title_en: 'NoNetwork'
+            // },
+            // #endif
+            // {
+            //     path: '/pages/componentsA/grid/grid',
+            //     icon: 'ca-grid',
+            //     title: 'Grid 宫格布局',
+            //     title_en: 'Grid'
+            // },
+            // {
+            //     path: '/pages/componentsC/swiper/swiper',
+            //     icon: 'ca-swiper',
+            //     title: 'Swiper 轮播图',
+            //     title_en: 'Swiper'
+            // },
+            // {
+            //     path: '/pages/componentsC/skeleton/skeleton',
+            //     icon: 'ca-skeleton',
+            //     title: 'Skeleton 骨架屏',
+            //     title_en: 'Skeleton'
+            // },
+            // {
+            //     path: '/pages/componentsA/sticky/sticky',
+            //     icon: 'ca-sticky',
+            //     title: 'Sticky 吸顶',
+            //     title_en: 'Sticky'
+            // },
+            // {
+            //     path: '/pages/componentsA/divider/divider',
+            //     icon: 'ca-divider',
+            //     title: 'Divider 分割线',
+            //     title_en: 'Divider'
+            // }
         ]
     },
     {
@@ -365,18 +365,18 @@ export default [
                 title: 'Tabbar 底部导航栏',
                 title_en: 'Tabbar'
             },
-            {
-                path: '/pages/componentsA/backtop/backtop',
-                icon: 'ca-backTop',
-                title: 'BackTop 返回顶部',
-                title_en: 'BackTop'
-            },
-            {
-                path: '/pages/componentsC/navbar/navbar',
-                icon: 'ca-navbar',
-                title: 'Navbar 导航栏',
-                title_en: 'Navbar'
-            },
+            // {
+            //     path: '/pages/componentsA/backtop/backtop',
+            //     icon: 'ca-backTop',
+            //     title: 'BackTop 返回顶部',
+            //     title_en: 'BackTop'
+            // },
+            // {
+            //     path: '/pages/componentsC/navbar/navbar',
+            //     icon: 'ca-navbar',
+            //     title: 'Navbar 导航栏',
+            //     title_en: 'Navbar'
+            // },
             {
                 path: '/pages/componentsC/tabs/tabs',
                 icon: 'ca-tabs',
@@ -391,18 +391,18 @@ export default [
             // title_en: 'TabsSwiper',
             // },
             // // #endif
-            {
-                path: '/pages/componentsC/subsection/subsection',
-                icon: 'ca-subsection',
-                title: 'Subsection 分段器',
-                title_en: 'Subsection'
-            },
-            {
-                path: '/pages/componentsC/indexList/indexList',
-                icon: 'ca-indexList',
-                title: 'IndexList 索引列表',
-                title_en: 'IndexList'
-            },
+            // {
+            //     path: '/pages/componentsC/subsection/subsection',
+            //     icon: 'ca-subsection',
+            //     title: 'Subsection 分段器',
+            //     title_en: 'Subsection'
+            // },
+            // {
+            //     path: '/pages/componentsC/indexList/indexList',
+            //     icon: 'ca-indexList',
+            //     title: 'IndexList 索引列表',
+            //     title_en: 'IndexList'
+            // },
             {
                 path: '/pages/componentsC/steps/steps',
                 icon: 'ca-steps',
@@ -426,61 +426,61 @@ export default [
                 icon: 'ca-parse',
                 title: 'Parse 富文本解析器',
                 title_en: 'Parse'
-            },
-            {
-                path: '/pages/componentsC/codeInput/codeInput',
-                icon: 'ca-messageInput',
-                title: 'CodeInput 验证码输入',
-                title_en: 'CodeInput'
-            },
+            }
+            // {
+            //     path: '/pages/componentsC/codeInput/codeInput',
+            //     icon: 'ca-messageInput',
+            //     title: 'CodeInput 验证码输入',
+            //     title_en: 'CodeInput'
+            // },
             // {
             // path: '/pages/componentsC/avatarCropper/avatarCropper',
             // icon: 'ca-avatarCropper',
             // title: 'AvatarCropper 头像裁剪（暂无）',
             // title_en: 'AvatarCropper',
             // },
-            {
-                path: '/pages/componentsC/loadmore/loadmore',
-                icon: 'ca-loadmore',
-                title: 'Loadmore 加载更多',
-                title_en: 'Loadmore'
-            },
-            {
-                path: '/pages/componentsC/readMore/readMore',
-                icon: 'ca-readMore',
-                title: 'ReadMore 展开阅读更多',
-                title_en: 'ReadMore'
-            },
+            // {
+            //     path: '/pages/componentsC/loadmore/loadmore',
+            //     icon: 'ca-loadmore',
+            //     title: 'Loadmore 加载更多',
+            //     title_en: 'Loadmore'
+            // },
+            // {
+            //     path: '/pages/componentsC/readMore/readMore',
+            //     icon: 'ca-readMore',
+            //     title: 'ReadMore 展开阅读更多',
+            //     title_en: 'ReadMore'
+            // },
             // {
             // path: '/pages/componentsA/lazyLoad/lazyLoad',
             // icon: 'ca-lazyLoad',
             // title: 'LazyLoad 懒加载（暂无）',
             // title_en: 'LazyLoad',
             // },
-            {
-                path: '/pages/componentsA/gap/gap',
-                icon: 'ca-gap',
-                title: 'Gap 间隔槽',
-                title_en: 'Gap'
-            },
-            {
-                path: '/pages/componentsC/avatar/avatar',
-                icon: 'ca-avatar',
-                title: 'Avatar 头像',
-                title_en: 'Avatar'
-            },
-            {
-                path: '/pages/componentsA/link/link',
-                icon: 'ca-link',
-                title: 'Link 超链接',
-                title_en: 'Link'
-            },
-            {
-                path: '/pages/componentsA/transition/transition',
-                icon: 'ca-transition',
-                title: 'transition 动画',
-                title_en: '动画'
-            }
+            // {
+            //     path: '/pages/componentsA/gap/gap',
+            //     icon: 'ca-gap',
+            //     title: 'Gap 间隔槽',
+            //     title_en: 'Gap'
+            // },
+            // {
+            //     path: '/pages/componentsC/avatar/avatar',
+            //     icon: 'ca-avatar',
+            //     title: 'Avatar 头像',
+            //     title_en: 'Avatar'
+            // },
+            // {
+            //     path: '/pages/componentsA/link/link',
+            //     icon: 'ca-link',
+            //     title: 'Link 超链接',
+            //     title_en: 'Link'
+            // },
+            // {
+            //     path: '/pages/componentsA/transition/transition',
+            //     icon: 'ca-transition',
+            //     title: 'transition 动画',
+            //     title_en: '动画'
+            // }
         ]
     }
 ]
